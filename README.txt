3c4078cff1c9f6cb6bb61c219cbbd9c2b5eaf15b
Abdullah Kayes <imrul.kayesh@leads-bd.com>
2026-01-13 12:16:59 +0600

    added initial pwa app

---
+++ README.txt
@@ -0,0 +1,19 @@
+PWA Icons Implementation Guide
+
+1. Copy all pwa-*.png files to your web app's public directory
+2. Add the manifest.json code to your PWA manifest
+3. Include the HTML meta tags in your index.html <head>
+
+Sizes Included:
+- 512x512: Large PWA icon
+- 384x384: Medium PWA icon  
+- 256x256: Small PWA icon
+- 192x192: Standard PWA icon
+- 180x180: Apple touch icon
+- 152x152: iPad touch icon
+- 144x144: MS tile icon
+- 128x128: Chrome Web Store
+- 96x96: Favicon large
+- 72x72: Android launcher
+
+Implementation code is included in implementation.html
---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "daruchini-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./jspdf.umd.min.js",
+  "./manifest.json",
+  "./pwa-72x72.png",
+  "./pwa-96x96.png",
+  "./pwa-128x128.png",
+  "./pwa-144x144.png",
+  "./pwa-152x152.png",
+  "./pwa-180x180.png",
+  "./pwa-192x192.png",
+  "./pwa-256x256.png",
+  "./pwa-384x384.png",
+  "./pwa-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
